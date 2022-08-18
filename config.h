6045fcb76be7d61eaae147d835bffef90eba9402
Adrian Christian Santos <adrian.crsantos@gmail.com>
2022-08-18 19:30:37 +0800

    Added scratchpad patch

--- config.h
+++ config.h
@@ -96,7 +96,9 @@ static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont,
 static const char *monitor[] = { "/usr/bin/htop", NULL };
 //sets st as the default terminal
 static const char *termcmd[]  = { "st", NULL };
-//sets urxvt as the default terminal
+static const char scratchpadname[] = "scratchpad";
+static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
+//sets st as the default terminal
 //volume controls
 static const char *upvol[]   = { "amixer", "-q", "set", "Master", "5%+", "unmute", NULL };
 static const char *downvol[] = { "amixer", "-q", "set", "Master", "5%-", "unmute", NULL };
@@ -108,6 +110,7 @@ static Key keys[] = {
 	/* modifier                     key              function                argument */
 	{ MODKEY,                       XK_d,            spawn,                  {.v = dmenucmd } },
 	{ MODKEY,	                XK_Return,       spawn,                  {.v = termcmd } },
+	{ MODKEY|ShiftMask,             XK_Return,       togglescratch,          {.v = scratchpadcmd } },
 	{ MODKEY,                       XK_t,            togglebar,              {0} },
 	{ MODKEY,                       XK_j,            focusstack,             {.i = +1 } },
 	{ MODKEY,                       XK_k,            focusstack,             {.i = -1 } },
