dee0ebec8e1f575add928d1f8786cbe3fe93ad29
Adrian Christian Santos <adrian.crsantos@gmail.com>
2023-11-22 23:08:37 +0800

    Set gap size between windows to 0

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 #include <X11/XF86keysym.h>
 /* appearance */
 static const unsigned int borderpx = 1; /* border pixel size of windows */
-static const unsigned int gappx = 2;    /* gaps size between windows */
+static const unsigned int gappx = 0;    /* gaps size between windows */
 static const unsigned int snap = 5;     /* snap pixel */
 static const unsigned int systraypinning =
     0; /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor
